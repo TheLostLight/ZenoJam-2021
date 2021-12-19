cee6cacfab068d2324a84a3a37f0854c069a157d
TheLostLight <seemydolphin@gmail.com>
2021-12-18 19:54:28 -0800

    Small code cleanups

--- src/Path2D.gd
+++ src/Path2D.gd
@@ -7,10 +7,10 @@ var moveSpeed = 300
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if pathBlocked == false:
+	if not pathBlocked:
 		path.set_offset(path.get_offset() + moveSpeed * delta)
 	else:
-		return
+		pass
 
 
 

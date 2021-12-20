92ddd81ae1dc3092e2f8998105a09092aba6f879
TheLostLight <seemydolphin@gmail.com>
2021-12-19 16:29:28 -0800

    Added background to defeat screen

--- src/Level1.gd
+++ src/Level1.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Input.set_custom_mouse_cursor(load("res://Assets/Sprites/Pointer.gif"))
+	pass
 
 func _on_DefeatTimer_timeout():
 	SceneManager.goToScene("DefeatScreen.tscn", false)
